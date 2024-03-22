b401e72d521ccf6449232df4d7e0cd0817e0fe0e
Henry Moreira <henrymoreirasilva@gmail.com>
2024-03-21 22:14:41 -0300

    Desafio 1 em desenvolvimento

--- desafios/client-server-api/server/main.go
+++ desafios/client-server-api/server/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
-	"strconv"
 	"time"
-
-	"github.com/google/uuid"
-	_ "github.com/mattn/go-sqlite3"
+	// "github.com/google/uuid"
+	// _ "github.com/mattn/go-sqlite3"
 )
 
 type Registro struct {
@@ -35,8 +32,10 @@ type Cotacao struct {
 }
 
 func main() {
+
 	http.HandleFunc("/cotacao", GetCotacao)
 	http.ListenAndServe(":8080", nil)
+
 }
 
 func GetCotacao(w http.ResponseWriter, r *http.Request) {
@@ -46,76 +45,112 @@ func GetCotacao(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// contexto HTTP
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*200)
-	defer cancel()
+	ctx := r.Context()
+	log.Println("start")
+	defer log.Println("end")
+
+	select {
+	case <-time.After(1000 * time.Millisecond):
+		log.Println("200 milissegundos")
+		CotacaoDolar, err := requestCotacao()
+		if err != nil {
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+		w.Write([]byte(CotacaoDolar.Usdbrl.Bid))
+	case <-ctx.Done():
+		log.Println("Candelado pelo cliente")
+	}
 
 	// requisição
-	req, err := http.NewRequestWithContext(ctx, "get", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	//	defer req.Body.Close()
+	// req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	// if err != nil {
+	// 	panic(err)
+	// 	//w.WriteHeader(http.StatusBadRequest)
+	// 	//return
+	// }
+	// defer req.Body.Close()
+	// 	log.Println("2")
+	// 	resp, err := http.DefaultClient.Do(req)
+	// 	if err != nil {
+	// 		w.WriteHeader(http.StatusForbidden)
+	// 		return
+	// 	}
+	// 	log.Println("3")
+	// 	// leitura
+	// 	res, err := io.ReadAll(resp.Body)
+	// 	if err != nil {
+	// 		w.WriteHeader(http.StatusExpectationFailed)
+	// 		return
+	// 	}
+	// 	log.Println("4")
+	// 	// conversão
+	// 	var CotacaoDolar Cotacao
+	// 	err = json.Unmarshal(res, &CotacaoDolar)
+	// 	if err != nil {
+	// 		w.WriteHeader(http.StatusConflict)
+	// 		return
+	// 	}
 
-	resp, err := http.DefaultClient.Do(req)
+	// 	w.Write([]byte(CotacaoDolar.Usdbrl.Bid))
+
+	// 	// registro
+	// 	// floatValue, _ := strconv.ParseFloat(CotacaoDolar.Usdbrl.Bid, 64)
+	// 	// registro := NewRegistro(floatValue)
+	// 	// _ = insertRegistro(registro)
+
+	// }
+
+	// func NewRegistro(valor float64) *Registro {
+	// 	return &Registro{
+	// 		ID:    uuid.New().String(),
+	// 		Valor: valor,
+	// 	}
+	// }
+
+	// func insertRegistro(registro *Registro) error {
+
+	// 	db, err := sql.Open("sqlite3", "cotacoes.db")
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	defer db.Close()
+
+	// 	// _, err = db.Exec("create table if not exists cotacoes (id string NOT NULL PRIMARY KEY, valor float NOT NULL)")
+	// 	// if err != nil {
+	// 	// 	return err
+	// 	// }
+
+	// 	stmt, err := db.Prepare("insert into cotacoes (id, valor) values ($1, $2)")
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	defer stmt.Close()
+	// 	_, err = stmt.Exec(registro.ID, registro.Valor)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+
+	// 	return nil
+
+}
+
+func requestCotacao() (*Cotacao, error) {
+	resp, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return nil, err
 	}
 
-	// leitura
 	res, err := io.ReadAll(resp.Body)
 	if err != nil {
-		w.WriteHeader(http.StatusExpectationFailed)
-		return
+		return nil, err
 	}
 
-	// conversão
 	var CotacaoDolar Cotacao
 	err = json.Unmarshal(res, &CotacaoDolar)
 	if err != nil {
-		w.WriteHeader(http.StatusExpectationFailed)
-		return
+		return nil, err
 	}
 
-	w.Write([]byte(CotacaoDolar.Usdbrl.Bid))
-
-	// registro
-	floatValue, _ := strconv.ParseFloat(CotacaoDolar.Usdbrl.Bid, 64)
-	registro := NewRegistro(floatValue)
-	_ = insertRegistro(registro)
-
-}
-
-func NewRegistro(valor float64) *Registro {
-	return &Registro{
-		ID:    uuid.New().String(),
-		Valor: valor,
-	}
-}
-
-func insertRegistro(registro *Registro) error {
-	db, err := sql.Open("sqlite3", "cotacoes.db")
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	_, err = db.Query("create table if not exists cotacoes (id string NOT NULL PRIMARY KEY, valor float NOT NULL)")
-	if err != nil {
-		return err
-	}
-
-	stmt, err := db.Prepare("insert into cotacoes (id, valor) values (?, ?)")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec(registro.ID, registro.Valor)
-	if err != nil {
-		return err
-	}
-	return nil
-
+	return &CotacaoDolar, nil
 }
