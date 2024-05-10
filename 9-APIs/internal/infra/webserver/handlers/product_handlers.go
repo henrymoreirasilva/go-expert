4cebf386a05db4b66f746d31d5eb5d3775f4a4db
Henry Moreira <henrymoreirasilva@gmail.com>
2024-05-10 17:47:46 -0300

    CRUD de produtos concluído

--- 9-APIs/internal/infra/webserver/handlers/product_handlers.go
+++ 9-APIs/internal/infra/webserver/handlers/product_handlers.go
@@ -3,11 +3,13 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi"
 	"github.com/henrymoreirasilva/go-expert/9-APIs/internal/dto"
 	"github.com/henrymoreirasilva/go-expert/9-APIs/internal/entity"
 	"github.com/henrymoreirasilva/go-expert/9-APIs/internal/infra/database"
+	entityPkg "github.com/henrymoreirasilva/go-expert/9-APIs/pkg/entity"
 )
 
 type ProductHandle struct {
@@ -59,4 +61,83 @@ func (h *ProductHandle) GetProduct(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(product)
+}
+
+func (h *ProductHandle) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var product entity.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product.ID, err = entityPkg.ParseID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	println(product.ID.String())
+	err = h.ProductDB.Update(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
+
+}
+
+func (h *ProductHandle) GetProducts(w http.ResponseWriter, r *http.Request) {
+	page := chi.URLParam(r, "page")
+	limit := chi.URLParam(r, "limit")
+	sort := chi.URLParam(r, "sort")
+
+	pageInt, err := strconv.Atoi(page)
+	if err != nil {
+		pageInt = 0
+	}
+
+	limitInt, err := strconv.Atoi(limit)
+	if err != nil {
+		limitInt = 0
+	}
+
+	products, err := h.ProductDB.FindAll(pageInt, limitInt, sort)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(products)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func (h *ProductHandle) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err := h.ProductDB.Delete(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
 }
